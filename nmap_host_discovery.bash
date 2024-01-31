19e5dde2e7589b42a94764ff2826197e24ca1a07
soup-hacker <miles.campbell@mymail.champlain.edu>
2024-01-30 20:18:33 -0500

    2

--- nmap_host_discovery.bash
+++ nmap_host_discovery.bash
@@ -2,7 +2,7 @@
 
 filename="sweep_3.txt"
 
-date | tee -a "./$filename"
+#date | tee -a "./$filename"
 
 for i in {2..50}; do
 	nmap -sn 10.0.5.$i | grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | tee -a "./$filename"
--- ping_host_discovery.bash
+++ ping_host_discovery.bash
@@ -2,7 +2,7 @@
 
 filename="sweep.txt"
 
-date | tee -a "./$filename"
+#date | tee -a "./$filename"
 
 for i in {2..50}; do
 	ping -c 1 10.0.5.$i | grep 'bytes from' | awk '{ print $4 }' | tr -d ':' | tee -a "./$filename"
