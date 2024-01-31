ff9734fe777af44cfa19414d7d6398b5aaead479
soup-hacker <miles.campbell@mymail.champlain.edu>
2024-01-31 09:08:26 -0500

    text

--- portscanner.bash
+++ portscanner.bash
@@ -2,10 +2,13 @@
 hostfile=$1
 portfile=$2
 
-#echo "host,port"
-
-for host in $(cat $hostfile); do 
-	for port in $(cat $hostfile); do
-		timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && echo "$host $port"
+if [ -f $hostfile ] && [ -f $portfile ]; then
+	for host in $(cat $hostfile); do 
+		for port in $(cat $portfile); do
+			# output has been changed from CSV to just seperated by a space character
+			timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && echo "$host $port" | tee -a "./port_scan_results.txt"
+		done
 	done
-done
+else
+   	echo 'One of the files does not exist.'
+fi
