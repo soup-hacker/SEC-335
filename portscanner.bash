78a5016b180b7524ad7a55ab038c61a6024b83e5
soup-hacker <miles.campbell@mymail.champlain.edu>
2024-02-05 17:31:02 -0500

    Update portscanner.bash

--- portscanner.bash
+++ portscanner.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 hostfile=$1
 portfile=$2
+filename=$3
 
 if [ -f $hostfile ] && [ -f $portfile ]; then
+	echo "IP Port" >> $filename
 	for host in $(cat $hostfile); do 
 		for port in $(cat $portfile); do
 			# output has been changed from CSV to just seperated by a space character
-			timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && echo "$host $port" | tee -a "./port_scan_results.txt"
+			timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && echo "$host $port" | tee -a $filename
 		done
 	done
 else
